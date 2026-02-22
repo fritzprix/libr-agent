e55682e526a1a059e74d0dffe069c72a10045970
fritzprix <innocentevil0914@gmail.com>
2026-02-22 19:21:59 +0900

    refactor: remove unused test files for path components

--- test_path.rs
+++
@@ -1,6 +0,0 @@
-use std::path::Path;
-
-fn main() {
-    let p = Path::new("/a/./b");
-    println!("{:?}", p.components().collect::<Vec<_>>());
-}
--- test_path_2.rs
+++
@@ -1,6 +0,0 @@
-use std::path::Path;
-
-fn main() {
-    println!(".: {:?}", Path::new("./foo").components().collect::<Vec<_>>());
-    println!("..: {:?}", Path::new("/a/../b").components().collect::<Vec<_>>());
-}
