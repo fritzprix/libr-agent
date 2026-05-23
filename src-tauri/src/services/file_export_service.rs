66ad105cf460b1fbfa26b696dce62bb054d39edf
fritzprix <innocentevil0914@gmail.com>
2026-05-23 11:46:46 +0900

    style: format file export test assertions to comply with rustfmt

--- src-tauri/src/services/file_export_service.rs
+++ src-tauri/src/services/file_export_service.rs
@@ -210,19 +210,37 @@ mod tests {
 
     #[test]
     fn test_sanitize_package_name_safe_inputs() {
-        assert_eq!(FileExportService::sanitize_package_name("my-awesome-package"), "my-awesome-package");
-        assert_eq!(FileExportService::sanitize_package_name("package_name_123"), "package_name_123");
+        assert_eq!(
+            FileExportService::sanitize_package_name("my-awesome-package"),
+            "my-awesome-package"
+        );
+        assert_eq!(
+            FileExportService::sanitize_package_name("package_name_123"),
+            "package_name_123"
+        );
     }
 
     #[test]
     fn test_sanitize_package_name_directory_traversal() {
-        assert_eq!(FileExportService::sanitize_package_name("../../../evil"), "_________evil");
-        assert_eq!(FileExportService::sanitize_package_name("..\\..\\..\\evil"), "_________evil");
+        assert_eq!(
+            FileExportService::sanitize_package_name("../../../evil"),
+            "_________evil"
+        );
+        assert_eq!(
+            FileExportService::sanitize_package_name("..\\..\\..\\evil"),
+            "_________evil"
+        );
     }
 
     #[test]
     fn test_sanitize_package_name_special_characters() {
-        assert_eq!(FileExportService::sanitize_package_name("package!@#name"), "package___name");
-        assert_eq!(FileExportService::sanitize_package_name("  trimmed package  "), "__trimmed_package__");
+        assert_eq!(
+            FileExportService::sanitize_package_name("package!@#name"),
+            "package___name"
+        );
+        assert_eq!(
+            FileExportService::sanitize_package_name("  trimmed package  "),
+            "__trimmed_package__"
+        );
     }
 }
